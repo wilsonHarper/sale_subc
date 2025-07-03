1dc069ac29f31710f69a0d2e8bf56d02705b67a5
Wilsonsrl <wilsonmunoz032@gmail.com>
2025-07-02 20:54:07 -0400

    odoo subsc

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Sale Subscription - No Draft Invoice Validation",
+    "version": "17.0.1.0.0",
+    "summary": "Permite generar facturas recurrentes aunque existan borradores.",
+    "depends": [
+        "sale_subscription", 
+        "sale_management",
+        "account"],
+    "data": [
+    ],
+    "author": "Wilson / Harper SRL",
+    "category": "Sales",
+    "license": "LGPL-3",
+    "installable": True,
+    "application": False
+}
